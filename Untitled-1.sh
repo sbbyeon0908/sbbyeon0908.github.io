e2f570ba624d827787eac288c35feb7ab64bdb3f
sbbyeon0908 <sbbyeon@hs.ac.kr>
2025-09-11 09:59:10 +0000

    이름작성

---
+++ Untitled-1.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/v0.39.7/install.sh | bash
+
+export NVM_DIR="$HOME/ .nvm"
+[ -s "$NVM__DIR/nvm.sh" ] && \. "%NVM_DIR/nvm.sh"
+nvm install mode
+
+node -v
+npm -v
